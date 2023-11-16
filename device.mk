3c638992c8e416980411827a28cd0f8250cc6f00
Legendleo90 <akshatleo9456@gmail.com>
2023-11-16 17:27:36 +0530

    beryllium: RisingTechOSS bringup

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-rising
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
--- rising_beryllium.mk
+++ rising_beryllium.mk
@@ -1,19 +1,29 @@
 #
-# Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) The RisingTechOSS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RisingOS stuff.
+$(call inherit-product, vendor/rising/config/rising.mk)
+
+# RisingOS flags
+RISING_MAINTAINER := AKSHAT
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Device Flags
+INLINE_KERNEL_BUILDING := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := rising_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := POCO F1
+PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := beryllium
