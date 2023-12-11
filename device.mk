c6ce9ac460cc276699c1df12643ef99c09864277
Joker-V2 <nipinna0@gmail.com>
2023-12-11 22:39:32 +0530

    beryllium: Build android.hardware.bluetooth.audio-impl
    W audiohalservice: Could not register Bluetooth Audio Offload API
      E audiohalservice: Failed to dlopen android.hardware.bluetooth.audio-impl.so: dlopen failed: library "android.hardware.bluetooth.audio-impl.so" not found
      W audiohalservice: createIBluetoothAudioProviderFactory() from android.hardware.bluetooth.audio-impl failed
    
    Signed-off-by: Shikhar <krtonia@gmail.com>
    Signed-off-by: Akshat Singh <akshatleo9456@gmail.com>

--- device.mk
+++ device.mk
@@ -97,6 +97,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
